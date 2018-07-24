a45c55627df57d82ba27afba7122f5767d64f137
Alexander Zvaniga <alex.zvaniga@gmail.com>
2018-07-24 21:07:35 +0000

    added peson added

--- add_person.js
+++ add_person.js
@@ -1,6 +1,6 @@
-const settings = require("./settings"); // settings.json
-const knex = require("knex")({
-	client: "pg",
+const settings = require('./settings'); // settings.json
+const knex = require('knex')({
+	client: 'pg',
 	connection: {
 		host: settings.hostname,
 		user: settings.user,
@@ -8,4 +8,13 @@ const knex = require("knex")({
 		database: settings.database
 	}
 });
-knex.
+knex('famous_people')
+	.insert({
+		first_name: process.argv[2],
+		last_name: process.argv[3],
+		birthdate: process.argv[4]
+	})
+	.then(function() {
+		knex.destroy();
+		return null;
+	});
